0695916777bf6fb380f63647a0c09dba6de0aa7f
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2026-01-03 20:30:14 +0000

    Add theme mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,45 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
-import {ThemeProvider} from "next-themes";
+import {ProjectProvider} from "@/context/useProject";
+import {ThemeProvider} from "@/components/theme-provider";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "designmatch",
-  description: "Compare your design side-by-side.",
+    title: "designmatch",
+    description: "Compare your design side-by-side.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-      >
-          {children}
-      </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <ProjectProvider>
+                {children}
+            </ProjectProvider>
+        </ThemeProvider>
+        </body>
+        </html>
+    );
 }
